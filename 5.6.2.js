e1900562b5ee20fa4a4d3ec77e7f9119ad72521f
rebemartinezgr <r.martinezgr@alumnos.urjc.es>
2015-02-26 12:14:07 +0100

    entrega

--- 5.6.2.js
+++ 5.6.2.js
@@ -1,8 +1,21 @@
+//function handlerIn(){
+	//$("#orderedlist > li:last")
+//};
 
+//function handlerOut(){
+	//$("#orderedlist > li:last")
+	//console.log("sale");
+//};
 
 $(document).ready(function() {
     $("#orderedlist2").addClass("blue");
     $("#orderedlist2 > li").addClass("red");
-    
+    	$("#orderedlist2 > li:last").hover( function(){
+		$(this).addClass("green");
+	}, function(){
+		$(this).removeClass("green");
+	});
 });
+
+
 
